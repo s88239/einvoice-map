08ac113a128ce2a8f2d76e787704622829e5cc2b
pcjeff <zendo3464@gmail.com>
2015-06-09 22:56:27 +0800

    Merge with parsing

--- mysite/py_lib/seller.py
+++ mysite/py_lib/seller.py
@@ -4,13 +4,30 @@ class Seller(object):
 	def __init__(self, _id, store_name, address, longitude, latitude):
 		self.id = int(_id)
 		self.store_name = store_name if store_name.find("-") == -1 else store_name[:store_name.find("-")]
-		self.brance_name = "" if store_name.find("-") == -1 else store_name[store_name.find("-")+1:]
+		self.branch_name = "" if store_name.find("-") == -1 else store_name[store_name.find("-")+1:]
 		self.address = address
 		self.longitude = longitude
 		self.latitude = latitude
-		self.cluster
+
+		#for user
+		self.visit_frequency = 0
+		self.consumption = 0.0
+		self.top_item = ''
+
+	def set_branch_name(self, n):
+		self.branch_name = n
+
+	def set_visit_frequency(self, f):
+		self.visit_frequency = f
+
+	def set_consumption(self, c):
+		self.consumption = c
+		
+	def set_top_item(self, i):
+		self.top_item = i
+
 	def _print(self):
-		print(self.id, self.store_name, self.brance_name, self.address, self.longitude, self.latitude)
+		print(self.id, self.store_name, self.branch_name, self.address, self.longitude, self.latitude, self.visit_frequency, self.consumption, self.top_item)
 
 def list_sellers(csv_file):
 	#key is the ID
@@ -23,7 +40,8 @@ def list_sellers(csv_file):
 		
 if __name__ == '__main__':
 	#TEST
-	sellers = list_sellers("../Taipei_shops_with_einvoice.csv")
+	all_sellers1 = list_sellers("/static/Taipei_shops_with_einvoice.csv")
+	#all_sellers2 = list_sellers("../Taipei_shops_with_einvoice.csv")
 	#for key in sellers:
 	for i in range(1, 2000):
 		sellers[i]._print()
--- mysite/py_lib/user.py
+++ mysite/py_lib/user.py
@@ -1,7 +1,17 @@
+import sys
+import einvoice
+from seller import list_sellers
+from seller import Seller
 
-from py_lib import einvoice
-from py_lib.invoice import Invoice
+try:
+	from trips.invoice import Invoice
+except:
+	from invoice import Invoice 
+	import pickle
 
+if sys.version_info >= (2, 7, 9):
+	import ssl
+	ssl._create_default_https_context = ssl._create_unverified_context
 
 class User(object):
 	def __init__(self, api_key, app_id, card_type, card_no, card_encrypt):
@@ -10,14 +20,64 @@ class User(object):
 		self.card_type = card_type
 		self.card_no = card_no
 		self.card_encrypt = card_encrypt
-		self.invoice_list = einvoice.get_einvoice(api_key, app_id, card_type, card_no, card_encrypt)
+		if not TEST:
+			self.invoice_list = einvoice.get_einvoice(api_key, app_id, card_type, card_no, card_encrypt)
+		else:
+			with open('invoice_list_tmp.pkl', 'rb') as f:
+				self.invoice_list = pickle.load(f)
 
-		#key is the ID of store
-		self.visit_freqency = {}
+
+		#key is the id of seller
+		self.sellers = {}
+		#key is the seller_name in invoice
+		self.visit_frequency = {}
 		self.top_item = {}
+		self.items = {}
 		self.consumption = {}
 
-#if __name__ == '__main__':
+	def add_seller(self, s):
+		i = s.branch_name
+		self.sellers[s.id] = Seller(s.id, s.store_name, s.address, s.longitude, s.latitude)
+		self.sellers[s.id].set_branch_name(s.branch_name)
+		self.sellers[s.id].set_visit_frequency(self.visit_frequency[i])
+		self.sellers[s.id].set_consumption(self.consumption[i])
+		self.sellers[s.id].set_top_item(self.top_item[i][0].description)
+		#self.sellers[s.id]._print()
+
+	def statistics(self, shop):
+		for invoice in self.invoice_list:
+			if invoice.seller_name not in self.visit_frequency:
+				self.visit_frequency[invoice.seller_name] = 0
+				self.items[invoice.seller_name] = {}
+				self.consumption[invoice.seller_name] = 0.0
+			self.visit_frequency[invoice.seller_name] += 1
+			for item in invoice.item:
+				if item not in self.items[invoice.seller_name]:
+					self.items[invoice.seller_name][item] = 0
+				self.items[invoice.seller_name][item] += 1
+			self.consumption[invoice.seller_name] += invoice.amount
+
+		for i in self.items:
+			top = 0  
+			for j in self.items[i]:
+				if top < self.items[i][j]:
+					top = self.items[i][j]
+			self.top_item[i] = [] 
+			for j in self.items[i]:
+				if self.items[i][j] >= top:
+					self.top_item[i].append(j)
+
+		#for i in self.visit_frequency:
+		#	print(i, self.visit_frequency[i], self.consumption[i], self.top_item[i][0].description)
+
+		sellers = list_sellers(shop)
+		for i in self.visit_frequency:
+			for j in sellers:
+				if i == sellers[j].branch_name:
+					self.add_seller(sellers[j])
+					#s = sellers[j]
+					#self.sellers[s.id] = Seller(s.id, s.store_name, s.branch_name, s.address, s.longitude, s.latitude)
+					#print(i, self.visit_frequency[i], self.consumption[i], self.top_item[i][0].description, sellers[j].address)
 
 def clustering(user):
 	import numpy as np
@@ -46,6 +106,7 @@ def clustering(user):
 	print("Number of Clusters : %d" % n_clusters_)
 
 	return user, sorted(user, key=lambda x:user[x].cluster)
+
 def login(account, password):
 	#TEST
 	api_key = "QWQ4dU9WMzRXa2xoYUdsZA=="
@@ -54,8 +115,22 @@ def login(account, password):
 	card_no = account
 	card_encrypt = password
 	user = User(api_key, app_id, card_type, card_no, card_encrypt)
+	all_sellers1 = list_sellers("/static/Taipei_shops_with_einvoice.csv")
 	(x, y) = clustering(user)
 	return x, y
+
+if __name__ == '__main__':
+	TEST = True
+	api_key = "QWQ4dU9WMzRXa2xoYUdsZA=="
+	app_id = "EINV0201505042102"
+	card_type = "3J0002"
+	card_no = '/SMV1EFQ'
+	card_encrypt = '1212'
+	user = User(api_key, app_id, card_type, card_no, card_encrypt)
+	all_sellers1 = list_sellers("/static/Taipei_shops_with_einvoice.csv")	
+	all_sellers1 = list_sellers("/static/Taipei_shops_with_einvoice.csv")	
+
 	#for inv in user.invoice_list:
 	#	inv._print()
-	#	print()
+
+	user.statistics('../Taipei_shops_with_einvoice.csv')
