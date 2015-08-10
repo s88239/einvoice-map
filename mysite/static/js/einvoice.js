477130350f0d32803d7803e3a102024605b6ca76
Eric Kuo <s88239@hotmail.com>
2015-08-11 02:03:42 +0800

    separate javascript from html to js file

---
+++ mysite/static/js/einvoice.js
@@ -0,0 +1,72 @@
+invoice_idx = shop_data[0].length-1; // 發票list位於商家list的位置
+item_idx = shop_data[0][invoice_idx][0].length-1; // list位於發票list的位置
+
+function get_invoice_list_string(invoice_array){
+    var items_array = invoice_array[invoice_idx][0][item_idx]; // 該張發票所有購買商品
+    var invoice_list = '<center><a href="javascript:showBlock(\'detail\', false)">close</a><h1><font color="blue">'
+    + invoice_array[3] + invoice_array[4]
+    + '</font></h1></center><table class="table"><tr><th>順序</th><th>日期</th><th>發票號碼</th><th>總金額</th><th>#</th><th>商品名稱</th><th>數量</th><th>單價</th><th>總價</th></tr>';
+    for(var i=0;i<invoice_array[invoice_idx].length;++i){ // 第幾張發票
+        var items_array = invoice_array[invoice_idx][i][item_idx]; // 該張發票所有購買商品
+        var item_num = items_array.length; // 該張發票商品筆數
+        var invoice_list = invoice_list  + '<tr><td rowspan="' + item_num + '">' + (i+1) + '</td>'; // 順序
+        for(var j=0;j<invoice_array[invoice_idx][i].length-1;++j){ // 日期 發票號碼 總金額
+            invoice_list = invoice_list + '<td rowspan="' + item_num + '">' + invoice_array[invoice_idx][i][j] + '</td>';
+        }
+        for(var j=0;j<item_num;++j){ // 購買品項 商品名稱 數量 單價 總價
+            if(j!=0) invoice_list += '<tr>';
+            for(var kk=0;kk<items_array[j].length;++kk){
+                invoice_list = invoice_list + '<td>' + items_array[j][kk] + '</td>';
+            }
+            invoice_list += '</tr>';
+        }
+    }
+    invoice_list += '</table>';
+    return invoice_list;
+}
+function show_all_shop(){
+    showBlock('detail',false);
+    showBlock('TGMap',false);
+    var invoice_list = '<center><p><font color="blue" size="+4">商店清單</font></p></center>'
+    + '<table class="table table-hover"><tr><th>順序</th><th>商店名稱</th><th>分店名稱</th><th>商店地址</th><th>頻率</th><th>消費金額</th><th>最常購買品項</th><th>cluster</th>';
+    for(i=0;i<data.length;++i){
+        invoice_list += '<tr><td>'+(i+1)+'</td>';
+        for(j=3;j<data[i].length-1;++j){
+            invoice_list += '<td>' + data[i][j] + '</td>';
+        }
+    }
+    invoice_list += '</table>';
+    document.getElementById('main_text').innerHTML = invoice_list;
+}
+function show_all_einvoice(){
+    showBlock('detail',false);
+    showBlock('TGMap',false);
+    var invoice_list = '<center><p><font color="blue" size="+4">電子發票清單</font></p></center>'
+    + '<table class="table table-hover"><tr><th>順序</th><th>消費日期</th><th>載具</th><th>商店名稱</th><th>消費金額</th><th>發票號碼</th>';
+    var count = 1;
+    for(i=einvoice_list.length-1;i>=0;--i,++count){
+        invoice_list += '<tr><td>' + count + '</td>'; // 順序
+        for(j=1;j<7;++j){
+            if(j==2){ // invoice.card_type
+                if(einvoice_list[i][j] == '3J0002') carrier_type = '手機條碼';
+                else if(einvoice_list[i][j] == '1K0001') carrier_type = '悠遊卡';
+                else if(einvoice_list[i][j] == '2G0001') carrier_type = 'iCash';
+                else carrier_type = '其他載具';
+                invoice_list += '<td>' + carrier_type + ' ' + einvoice_list[i][++j] + '</td>';
+            }
+            else{
+                invoice_list += '<td>' + einvoice_list[i][j] + '</td>';
+            }
+        }
+    }
+    invoice_list += '</table>';
+    document.getElementById('main_text').innerHTML = invoice_list;
+}
+function show_map_div(){
+    document.getElementById('main_text').innerHTML = '';
+    showBlock('detail',false);
+    showBlock('TGMap',true);
+}
+function showBlock(blockid, status){
+    document.getElementById(blockid).style.display = (status==true)?"block":"none";
+}
---
+++ mysite/static/js/tgos.js
@@ -0,0 +1,91 @@
+var pMap = null;
+function InitWnd() {
+    var pOMap = document.getElementById("TGMap");
+    pMap = new TGOS.TGOnlineMap(pOMap, TGOS.TGCoordSys.EPSG3857); // set coordinate to WGS84
+    pMap.setCenter(new TGOS.TGPoint(121.516946, 25.047941) );  // set the center of the initial map
+    pMap.setZoom(13); // Zooming scale
+    addPosition(data);
+}
+function getRandomColor() {
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+function getSymbol(color, freq){
+    size = (freq+10)*1.2; // set the size dending on the frequency of shopping times
+    pTGSymbo = new TGOS.TGSymbol(); // 建立幾何物件
+    pTGSymbo.symbolStyle = TGOS.TGSymbolStyle.TRIANGLE; // 設定標記的符號
+    pTGSymbo.xPixel = size; // 設定標記寬度
+    pTGSymbo.yPixel = size; // 設定標記高度
+    pTGSymbo.fillColor = color; // 設定填入顏色
+    pTGSymbo.fillOpacity = 0.7; // 設定透明度
+    pTGSymbo.strokeWeight = 2; // 設定框線寬度
+    pTGSymbo.strokeOpacity = 0.4; // 設定框線透明度
+    pTGSymbo.strokeColor = "#000000"; // 設定框線顏色
+    pTGSymbo.anchor.x = size/2; // 設定錨點x座標
+    pTGSymbo.anchor.y = size/2; // 設定錨點y座標
+    pTGSymbo.rotation = 0; // 設定符號旋轉方向
+    return pTGSymbo;
+}
+function addPosition(data){
+    var InfoWindowOptions = {
+         maxWidth:4000, //訊息視窗的最大寬度
+         pixelOffset: new TGOS.TGSize(5, -30), //InfoWindow起始位置的偏移量
+                                               //使用TGSize設定
+                                               //向右X為正, 向上Y為負
+         zIndex:99 //視窗堆疊順序
+    };
+    var multi_point = [];
+    var current_cluster = 1, current_color = "#AA2222";
+    symbol = getSymbol();
+    for(var i = 0; i < data.length; i++) {
+        if(current_cluster!=data[i][data[i].length-2]) current_color = getRandomColor();
+        var symbol = getSymbol(current_color,parseInt(data[i][6]));
+        console.log(data);
+        var point = new TGOS.TGPoint(parseFloat(data[i][1]),parseFloat(data[i][2])); // create its position
+        var pTGMarker = new TGOS.TGMarker(pMap,point,data[i][3]+'-'+data[i][4], symbol, {flat:false}); // establish the point on map
+
+        if(current_cluster==data[i][data[i].length-2]) multi_point.push(pTGMarker);
+        else{
+            addMarkerClusters(multi_point);
+            multi_point = [pTGMarker];
+            current_cluster = data[i][data[i].length-2];
+        }
+        //-----------------establish a message box--------------------
+        var InfoWindowOptions = {
+              maxWidth:4000, // 訊息視窗的最大寬度
+              pixelOffset: new TGOS.TGSize(5, -30) //InfoWindow起始位置的偏移量, 使用TGSize設定, 向右X為正, 向上Y為負
+        };
+        var invoice_message = data[i][3]+'-'+data[i][4]+'：'+data[i][5]+'<br />頻率：'+data[i][6]+'<br />總消費金額：'+data[i][7]+'<br />最常購買商品：'+data[i][8];
+        messageBox = new TGOS.TGInfoWindow(invoice_message, point, InfoWindowOptions); // the content in message box
+
+        TGOS.TGEvent.addListener(pTGMarker, "mouseover", function (pTGMarker, messageBox) { // when mouse over the point
+             return function () {
+                messageBox.open(pMap, pTGMarker);
+             }
+        } (pTGMarker, messageBox));
+
+        TGOS.TGEvent.addListener(pTGMarker, "mouseout", function (messageBox) { // when mouse out the point
+                 return function () {
+                            messageBox.close();
+                 }
+        } (messageBox));
+        TGOS.TGEvent.addListener(pTGMarker, "click", function (invoice_array){ // when click the point
+            return function (){
+                showBlock('detail',true);
+                document.getElementById('detail').innerHTML = get_invoice_list_string(invoice_array);
+                }
+            }(data[i]) );
+    }
+    addMarkerClusters(multi_point); // add these point to the same cluster
+}
+function addMarkerClusters(markers) {                           //使用群聚標記點功能
+    mCluster = new TGOS.TGMarkerCluster(pMap, markers,{}); //建立群聚標記點物件
+    mCluster.setMaxZoom(12);                             //設定群聚標記點最大縮放範圍
+    mCluster.setVisible(true);                         //設定群聚標記點是否為顯示狀態
+    mCluster.setSearchBounds(30);                      //設定群聚標記點的搜尋範圍(單位px)
+    mCluster.redrawAll(true);                         //是否重新繪製群聚標記點物件
+}
