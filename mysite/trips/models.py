edb030a036df53c1097d95d51a64ed5df14db38b
b00902073 <b00902073@ntu.edu.tw>
2015-08-13 20:42:10 +0800

    database test

--- mysite/trips/models.py
+++ mysite/trips/models.py
@@ -1,11 +1,58 @@
 from django.db import models
 
 # Create your models here.
-class Post(models.Model):
-	shop = models.CharField(max_length=100)
-	place = models.CharField(max_length=100)
-	longitude = models.FloatField()
-	latitude = models.FloatField()
-	
-	def __str__(self):
-		return self.shop
+class UserTable(models.Model):
+	api_key = models.CharField(max_length=100)
+	app_id = models.CharField(max_length=100)
+	card_type = models.CharField(max_length=100)
+	card_no = models.CharField(max_length=100)
+	card_encrypt = models.CharField(max_length=100)
+	carriers_keys = models.CharField(max_length=100)
+
+	invoice_keys = models.CharField(max_length=1000)
+	seller_keys = models.CharField(max_length=100)
+
+	def __unicode__(self):
+		return self.api_key + ' ' + app_id + ' ' + card_type + ' ' + card_no + ' ' + card_encrypt
+
+class CarrierTable(models.Model):
+	carrier_type = models.CharField(max_length=100)
+	carrier_id = models.CharField(max_length=100)
+	carrier_name = models.CharField(max_length=100)
+
+	def __unicode__(self):
+		return self.carrier_type + ' ' + self.carrier_id + ' ' + self.carrier_name
+
+class InvoiceTable(models.Model):
+	inv_num = models.CharField(max_length=100)
+	card_type = models.CharField(max_length=100)
+	card_no = models.CharField(max_length=100)
+	seller_name = models.CharField(max_length=100)
+	amount = models.CharField(max_length=100)
+	inv_date = models.CharField(max_length=100)
+	inv_preiod = models.CharField(max_length=100)
+	inv_status = models.CharField(max_length=100)
+	inv_donatable = models.CharField(max_length=100)
+	donate_mark = models.CharField(max_length=100)
+	inv_items = models.CharField(max_length=1000)
+
+	def __unicode__(self):
+		return self.inv_num
+
+class SellerTable(models.Model):
+	_id = models.CharField(max_length=100)
+	store_name = models.CharField(max_length=100)
+	branch_name = models.CharField(max_length=100)
+	address = models.CharField(max_length=100)
+	longitude = models.CharField(max_length=100)
+	latitude = models.CharField(max_length=100)
+
+	#for user
+	invoice_keys = models.CharField(max_length=100)
+	visit_frequency = models.CharField(max_length=100)
+	consumption = models.CharField(max_length=100)
+	top_item = models.CharField(max_length=100)
+	cluster = models.CharField(max_length=100)
+
+	def __unicode__(self):
+		return self._id
