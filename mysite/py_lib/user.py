6a68d69366b25925ea30776eaa716ad323423456
Eric Kuo <s88239@hotmail.com>
2015-08-23 06:54:50 +0800

    fix bug

--- mysite/py_lib/user.py
+++ mysite/py_lib/user.py
@@ -49,8 +49,7 @@ class User(object):
 		for inv in self.invoice_list:
 			for key, value in all_sellers.items():
 				(cur_store_name, cur_branch_name) = split_store_and_branch(inv.seller_name)	
-				if value.branch_name == cur_branch_name
-					and (cur_store_name=='' and test_store_name(value.store_name) or value.store_name in cur_store_name):
+				if value.branch_name == cur_branch_name	and (cur_store_name=='' and test_store_name(value.store_name) or value.store_name in cur_store_name):
 					inv.add_seller(value)
 					break
 			if inv.seller == None:
@@ -282,8 +281,7 @@ class User(object):
 			seller_on_csv = False
 			for csv_seller_key in all_sellers:
 				(cur_store_name, cur_branch_name) = split_store_and_branch(seller_name)
-				if all_sellers[csv_seller_key].branch_name == cur_branch_name
-					and (cur_store_name=='' and test_store_name(all_sellers[csv_seller_key].store_name)	or all_sellers[csv_seller_key].store_name in cur_store_name):
+				if all_sellers[csv_seller_key].branch_name == cur_branch_name and (cur_store_name=='' and test_store_name(all_sellers[csv_seller_key].store_name) or all_sellers[csv_seller_key].store_name in cur_store_name):
 					self.add_seller(all_sellers[csv_seller_key],seller_name)
 					self.sellers[all_sellers[csv_seller_key].id].invoice_list = tmp_invoice_list[seller_name]
 					seller_on_csv = True
